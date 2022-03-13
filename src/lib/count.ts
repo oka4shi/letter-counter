75b08c3197e437ed94893768f104bceddadd5745
goahi <goai@protonmail.com>
2022-03-13 17:49:50 +0900

    fix: replace replaceAll with replace

--- src/lib/count.ts
+++ src/lib/count.ts
@@ -5,15 +5,15 @@ const count_num = (text: string): number => [...text].length;
 const delete_spaces = (text: string): string => {
     const reg =
         /[\u0009-\u000d\u001c-\u0020\u11a3-\u11a7\u1680\u180e\u2000-\u200f\u202f\u205f\u2060\u3000\u3164\ufeff\u034f\u2028\u2029\u202a-\u202e\u2061-\u2063\ufeff]/gi;
-    return text.replaceAll(reg, "");
+    return text.replace(reg, "");
 };
 
 const count_words = (text: string): number => {
     const blanks =
         /[\u0009-\u000d\u001c-\u0020\u11a3-\u11a7\u1680\u180e\u2000-\u200f\u202f\u205f\u2060\u3000\u3164\ufeff\u034f\u2028\u2029\u202a-\u202e\u2061-\u2063\ufeff]/gi;
 
-    const normalized_blanks = text.replaceAll(blanks, " ");
-    const excluded_duplicates = normalized_blanks.replaceAll(/ {2,}/g, " ");
+    const normalized_blanks = text.replace(blanks, " ");
+    const excluded_duplicates = normalized_blanks.replace(/ {2,}/g, " ");
 
     return excluded_duplicates.split(" ").filter((v) => v).length;
 };
